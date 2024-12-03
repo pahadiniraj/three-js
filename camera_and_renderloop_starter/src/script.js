6eb2426467f1d9f041952cfa8445a7b8a389b200
pahadiniraj <sharma12345niraj@gmail.com>
2024-12-03 14:44:01 +0545

    reducing alias

--- camera_and_renderloop_starter/src/script.js
+++ camera_and_renderloop_starter/src/script.js
@@ -1,8 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-console.log(OrbitControls);
-
 // initialize the scene
 const scene = new THREE.Scene();
 
@@ -32,7 +30,13 @@ camera.position.z = 5;
 const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true,
 });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const maxPixelRatio = Math.min(window.devicePixelRatio, 2);
+renderer.setPixelRatio(maxPixelRatio);
 
 // instaniate the controls
 
@@ -41,6 +45,12 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.autoRotate = true;
 
+window.addEventListener("resize", (e) => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
 const renderloop = () => {
   controls.update();
   renderer.render(scene, camera);
@@ -48,5 +58,4 @@ const renderloop = () => {
 };
 
 renderloop();
-renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
